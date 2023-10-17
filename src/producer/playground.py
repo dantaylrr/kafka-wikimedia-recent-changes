b46db11e151c290af0dfcb44f740e1be3baf6ffe
dantaylrr <dannyyyyt@gmail.com>
2023-10-17 12:00:11 +0100

    Removing playground.

--- src/producer/playground.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('/Users/daniel.taylor/github-projects/kafka-wikimedia-recent-changes/tests/events/event.json', 'rb') as f:
-    content = json.loads(f.read())
-
-print(content)
